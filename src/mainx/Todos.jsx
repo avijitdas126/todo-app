6b4ce1d7bcce2df5d431029740c33fc05e43cfa6
Avijit Das <rdas9237@gmail.com>
2023-07-22 11:04:52 +0530

    New features is added

--- src/mainx/Todos.jsx
+++ src/mainx/Todos.jsx
@@ -13,15 +13,31 @@ export default function Todos(props) {
     }
     return a;
   }
-  let data = props.data;
+  let data3 = localStorage.getItem("completetodo");
+  if (data3.length > 0) {
+    data3 = JSON.parse(data3);
+  } else {
+    data3 = [];
+  }
 
+  const [complete, setcomplete] = useState(data3);
+
+  console.log(data3);
+
+  let data = props.data;
+  let data2 = localStorage.getItem("incompletetodo");
+
+  data2 = JSON.parse(data2);
+
+  console.log(data2);
+  console.log(data3);
   const [addnewTodo, setaddnewTodo] = useState({ id: password(), task: "" });
-  const [complete, setcomplete] = useState([]);
+
   const [edit, setedit] = useState(false);
   const [completestatus, setcompletestatus] = useState(false);
-  const [incomplete, setincomplete] = useState(data);
+  const [incomplete, setincomplete] = useState(data2);
   const { id, task } = addnewTodo;
- const [cob,setcob]=useState('')
+  const [cob, setcob] = useState("");
   const handleChange = (e) => {
     setaddnewTodo({ ...addnewTodo, [e.target.name]: e.target.value });
   };
@@ -43,8 +59,8 @@ export default function Todos(props) {
       setincomplete(data);
     }
   };
-  let [index,setindex] = useState('');
-  let [array,setarray] = useState('');
+  let [index, setindex] = useState("");
+  let [array, setarray] = useState("");
   const value3 = (data) => {
     setedit(data);
   };
@@ -53,8 +69,8 @@ export default function Todos(props) {
       setedit(true);
       let filtered = incomplete.filter((elem, index1, array1) => {
         if (elem.id == data) {
-       setindex(index1);
-       setarray(array1)
+          setindex(index1);
+          setarray(array1);
         }
         return elem.id == data;
       });
@@ -62,15 +78,23 @@ export default function Todos(props) {
         obj[index] = item;
         return obj;
       });
-setcob(result);
+      setcob(result);
     }
   };
-  let val=(data)=>{
+  {
+    localStorage.setItem("incompletetodo", JSON.stringify(incomplete));
+  }
+  {
+    localStorage.setItem("completetodo", JSON.stringify(complete));
+  }
+  // {
+  // setincomplete(JSON.parse(data2))}
+  let val = (data) => {
     let fil = incomplete.filter((elem) => {
       return elem.id != data;
     });
     setincomplete(fil);
-  }
+  };
   const value1 = (data) => {
     data = complete.filter((elem) => {
       return elem.id != data;
@@ -92,40 +116,34 @@ setcob(result);
   });
   const dat1 = [];
   complete.forEach((elem) => {
-    dat1.push(
-      <Complete
-        key={elem.id}
-        val={elem}
-        onid={value1}
-      />
-    );
+    dat1.push(<Complete key={elem.id} val={elem} onid={value1} />);
   });
   return (
     <>
-    <div className="box">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          incomplete.push(addnewTodo);
-          setaddnewTodo({ id: password(), task: "" });
-        }}
-      >
-        <h2>Todo Application</h2>
-        <label htmlFor="task">Task:</label>
-        <input
-          type="text"
-          required
-          placeholder="Add task"
-          onChange={handleChange}
-          name="task"
-          value={task}
-        />
-        <br />
-        <br />
-        <button>Add New Todo</button>
-      </form>
+      <div className="box">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            incomplete.push(addnewTodo);
+            setaddnewTodo({ id: password(), task: "" });
+          }}
+        >
+          <h2>Todo Application</h2>
+          <label htmlFor="task">Task:</label>
+          <input
+            type="text"
+            required
+            placeholder="Add task"
+            onChange={handleChange}
+            name="task"
+            value={task}
+          />
+          <br />
+          <br />
+          <button>Add New Todo</button>
+        </form>
       </div>
-     
+
       <div className="card">
         <div className="incomplete-card">
           <h1>
@@ -137,11 +155,10 @@ setcob(result);
           <h1>
             <u>complete tasks</u>
           </h1>
-          {completestatus && dat1}
-      
+          {dat1}
         </div>
-       
-        {edit && <Edit onedit={value3} index={index} elem={array} dat={cob}/>}
+
+        {edit && <Edit onedit={value3} index={index} elem={array} dat={cob} />}
       </div>
     </>
   );
